e0a413755e79f922291ea896b8853d8e17e460ae
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-09-27 14:38:00 +0100

    created test for a top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,5 +7,15 @@ describe Oystercard do
     expect(subject.balance).to eq 0
     end
   end
+ 
+  describe "#top_up" do
+    it { is_expected.to respond_to(:top_up).with(1).argument } 
+    it 'tops up the balance to a passed value' do 
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end
+  end
+  
+
     
 end
