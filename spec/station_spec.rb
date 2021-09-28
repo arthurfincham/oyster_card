2164207f7ac20e1b5b69dbfb1c6d6dcc550efb7b
Arthur Fincham <arthurfincham@me.com>
2021-09-28 16:00:55 +0100

    add Station class, name and zone attr

---
+++ spec/station_spec.rb
@@ -0,0 +1,23 @@
+require 'station'
+
+describe Station do
+
+  context '#attributes' do
+    
+    it 'has required attributes' do
+      station = Station.new('NHG', 2)
+      expect(station).to have_attributes(:name => 'NHG', :zone => 2)
+    end
+
+    it 'knows its name' do                      
+      station = Station.new('NHG', 2)
+      expect(station.name).to eq('NHG')              
+    end                                                   
+
+    it 'knows its zone' do
+      station = Station.new('NHG', 2)
+      expect(station.zone).to eq(2)                                 
+    end
+  end
+
+end
