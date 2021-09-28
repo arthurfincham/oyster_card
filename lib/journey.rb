86eee09ce81c5d859ac7726d19331e4b8b05b54e
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-09-28 16:29:56 +0100

    created Journey class file and required it in oystercard.rb

---
+++ lib/journey.rb
@@ -0,0 +1,8 @@
+class Journey
+  attr_accessor :exit_station
+
+  def initialize(entry_station) 
+    @entry_station = entry_station
+    @exit_station = nil
+  end
+end
--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,13 @@
+require_relative 'journey'
+
 class Oystercard
 
   attr_accessor :balance
-  attr_reader :entry_station, :journey_history
+  attr_reader :entry_station, :journey_history, :j
 
   LIMIT = 90
   FEE = 1 
+
 
   def initialize
     @balance = 0
@@ -24,11 +27,14 @@ class Oystercard
   def touch_in(station)
     raise 'Not enough money' if @balance < FEE
     @entry_station = station
+    @j = Journey.new(station)
+    
   end
 
   def touch_out(station)
     @journey_history.store(@entry_station, station)
     @entry_station = nil
+    @j.exit_station = station
     deduct
   end
 
