8ab5b0b41d3554a43ba6a3f62db3509c0d5d26f6
Arthur Fincham <arthurfincham@me.com>
2021-09-30 13:38:30 +0100

    create journey_log file and spec

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,27 +1,3 @@
-# class JourneyLog
+class JourneyLog
 
-#   attr_reader :journeys
-
-#   def initialize(journey_class: Journey)
-#     @journey_class = journey_class
-#     @journeys = []
-#   end
-
-#   def start(station)
-#     log(@journey_class.new(entry_station: station))
-#   end
-
-#   def finish(station)
-#     @journeys.last.exit_station = station
-#   end
-
-# private
-
-#   def log(journey)
-#     @journeys << journey
-#   end
-
-#   def current_journey
-#     @journeys.last ||= journey_class.new
-#   end
-# end
+end
