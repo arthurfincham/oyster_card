6be9cd749f547174f2a69a2caff0bb9f58e95178
Stewart-Alexander ANNANI <wxst.stewart@gmail.com>
2021-09-30 15:06:10 +0100

    added start and finish methods and their respective test in the journey_log_spec.rb file

--- lib/journey_log.rb
+++ lib/journey_log.rb
@@ -1,3 +1,18 @@
 class JourneyLog
+   
+  attr_reader :journeys  
+  
+  def initialize(journey_class: Journey)
+    @journey_class = journey_class
+    @journeys = []
+  end
+
+  def start(station)
+    @journeys << @journey_class.new(entry_station: station)
+  end
+
+  def finish(station)
+    @journeys.last.exit_station = station
+  end
 
 end
--- spec/journey_log_spec.rb
+++ spec/journey_log_spec.rb
@@ -1,6 +1,40 @@
 require 'journey_log'
 
 describe JourneyLog do
+    let(:journey){ double :journey }
+    let(:station){ double :station }
+    let(:exit_station){ double :exit_station }
+    let(:journey_class){ double :journey_class, new: journey}
+    subject {described_class.new(journey_class: journey_class)}
+    
+    context ".start" do
+      it 'starts a journey' do  
+        expect(journey_class).to receive(:new).with(entry_station: station)
+        subject.start(station)
+      end
+
+      it 'records a journey' do
+          allow(journey_class).to receive(:new).and_return journey
+          subject.start(station)
+          expect(subject.journeys).to include journey 
+      end
+    end 
+
+    context ".finish" do
+      it 'stops a journey' do
+        expect(journey_class).to receive(:new).with(entry_station: station)
+        subject.start(station)
+        subject.finish(exit_station)
+      end
+
+      it 'records a journey' do
+        allow(journey_class).to receive(:new).and_return journey
+        subject.start(station)
+        subject.finish(exit_station)
+        expect(subject.journeys).to include journey 
+      end
+    end
+ 
 
 
 end
